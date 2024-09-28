27d4cba400dadc2e6302841f0e306e44edaf6683
yelucc <413602487@qq.com>
2024-09-28 14:46:15 +0800

    feat(葵花): 排行榜滚动问题

--- src/views/KuiHua/mobile/dashboard/index.vue
+++ src/views/KuiHua/mobile/dashboard/index.vue
@@ -7,32 +7,23 @@
         <div class="btn" @click="router.push('/rule')"></div>
       </div>
       <div class="leaderboard">
-        <div class="item" v-for="(item,index) in leaderboard.slice(0,3)" :key="index">
-          <div class="left">
-            <div class="index" :style="{background: `url('src/assets/images/leaderboard_${index+1}.png') no-repeat center center`,backgroundSize:'cover'}"></div>
-            <div>{{ item.teamName }}</div>
-          </div>
-          <div class="right">
-            <div>{{ item.score }}</div>
-            <span class="point">
+        <div class="content">
+          <div class="item" v-for="(item,index) in leaderboard" :key="index">
+            <div class="left">
+              <div class="index" v-if="index <=2"></div>
+              <div class="index" v-else>{{ item.sortIndex }}</div>
+              <div>{{ item.teamName }}</div>
+            </div>
+            <div class="right">
+              <div>{{ item.score }}</div>
+              <span class="point">
               分
             </span>
-          </div>
-
-
-        </div>
-        <div class="item" v-for="(item,index) in leaderboard.slice(3)" :key="index">
-          <div class="left">
-            <div>{{ item.sortIndex }}</div>
-            <div>{{ item.teamName }}</div>
-          </div>
-          <div class="right">
-            <div>{{ item.score }}</div>
-            <span class="point">
-              分
-            </span>
+            </div>
           </div>
         </div>
+
+
       </div>
       <div class="join" @click="handleOnClickJoin"></div>
 
@@ -50,6 +41,7 @@ import KhLogin from "@/views/KuiHua/mobile/components/KhLogin.vue";
 import {getToken} from "@/utils/auth.js";
 import KhsubmitSharedDialog from "@/views/KuiHua/mobile/components/KhsubmitSharedDialog.vue";
 import {useRouter} from "vue-router";
+
 const router = useRouter()
 const leaderboard = ref([])
 const loginVisible = ref(false)
@@ -113,7 +105,7 @@ getLeaderboard()
     }
 
     .leaderboard {
-      height: 410px;
+      height: 400px;
       width: 100%;
       //border: #1ab394 solid 1px;
       background: url("@/assets/images/leaderboard.png") no-repeat top center;
@@ -121,49 +113,93 @@ getLeaderboard()
       margin: 15px 20px;
       padding: 10% 20px;
 
-      .item {
-        width: 100%;
-        background-color: rgba(235, 242, 252, 1);
-        border-radius: 8px;
-        height: 55px;
-        margin-bottom: 5px;
+      .content {
+        height: 350px;
+        overflow-y: auto;
 
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        flex-direction: row;
-
-        padding: 0 10px;
-
-        font-family: "HuaKang Yuan W7A",serif;
-        font-size: 20px;
-        color: #0A3279;
-        font-weight: 800;
-
-        .left {
-          display: flex;
-          align-items: center;
-          justify-content: flex-start;
-          width: 80%;
-
-          .index {
-            margin-right: 20px;
-            width: 50px;
-            height: 50px;
-
-          }
-        }
-        .right{
-          display: flex;
-          align-items: center;
-          justify-content: flex-start;
-          .point{
-            font-size: 14px;
-            margin-left: 5px;
-          }
+        &::-webkit-scrollbar {
+          display: none; // 隐藏滚动条
         }
 
+        .item:nth-child(1) {
+          .left {
+            .index {
+              background: url('src/assets/images/leaderboard_1.png') no-repeat center center;
+              background-size: cover;
+            }
+          }
+
+        }
+
+        .item:nth-child(2) {
+          .left {
+            .index {
+              background: url('src/assets/images/leaderboard_2.png') no-repeat center center;
+              background-size: cover;
+            }
+          }
+
+        }
+
+        .item:nth-child(3) {
+          .left {
+            .index {
+              background: url('src/assets/images/leaderboard_3.png') no-repeat center center;
+              background-size: cover;
+            }
+          }
+
+        }
+
+        .item {
+          width: 100%;
+          background-color: rgba(235, 242, 252, 1);
+          border-radius: 8px;
+          height: 55px;
+          margin-bottom: 5px;
+
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+          flex-direction: row;
+
+          padding: 0 10px;
+
+          font-family: "HuaKang Yuan W7A", serif;
+          font-size: 20px;
+          color: #0A3279;
+          font-weight: 800;
+
+          .left {
+            display: flex;
+            align-items: center;
+            justify-content: flex-start;
+            width: 80%;
+
+            .index {
+              margin-right: 20px;
+              width: 50px;
+              height: 50px;
+              text-align: center;
+              line-height: 50px;
+            }
+          }
+
+          .right {
+            display: flex;
+            align-items: center;
+            justify-content: flex-start;
+
+            .point {
+              font-size: 14px;
+              margin-left: 5px;
+            }
+          }
+
+        }
       }
+
+
     }
 
     .join {
